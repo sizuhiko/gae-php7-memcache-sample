5aee9f291e45de57a55ee01e9102929ac3721037
sizuhiko <k-kishida@esm.co.jp>
2019-01-07 15:54:52 +0900

    iniファイルを追加して接続情報を表示するようにした

--- index.php
+++ index.php
@@ -1,15 +1,24 @@
 <html>
   <body>
     <?php
-      $mem = new Memcached();
-      $mem->setOption(Memcached::OPT_BINARY_PROTOCOL, true);
-      $mem->addServer(getenv('MEMCACHED_SERVER_ADDR'), 11211);
-      $mem->setSaslAuthData(getenv('MEMCACHED_USERNAME'), getenv('MEMCACHED_PASSWORD'));
-      $mem->set("hello", "World");
+      try {
+        $mem = new Memcached();
+        $mem->setOption(Memcached::OPT_BINARY_PROTOCOL, true);
+        $mem->addServer(getenv('MEMCACHED_SERVER_ADDR'), 11211);
+        $mem->setSaslAuthData(getenv('MEMCACHED_USERNAME'), getenv('MEMCACHED_PASSWORD'));
 
-      echo 'Hello '.$mem->get("hello");
+        echo '<pre><code>';
+        print_r($mem->getStats());
+        echo '</code></pre>';
 
-      $mem->quit();
+        $mem->set("hello", "World");
+
+        echo 'Hello '.$mem->get("hello");
+
+        $mem->quit();
+      } catch(Exception $e) {
+        var_dump($e);
+      }
     ?>
   </body>
 </html>
